e618a679a10da0c6534bb18b8b9166ad8326adc6
FreakyCoder <kurayogun@gmail.com>
2020-06-28 22:35:08 +0300

    Finally boilerplate is ready

--- src/services/navigation/index.js
+++ src/services/navigation/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from "react-native-dynamic-vector-icons";
 
 // ? Screens
-import HomeScreen from "../../screens/home/HomeScreen";
-import SearchScreen from "../../screens/search/SearchScreen";
-import DetailScreen from "../../screens/detail/DetailScreen";
+import HomeScreen from "@screens/home/HomeScreen";
+import SearchScreen from "@screens/search/SearchScreen";
+import DetailScreen from "@screens/detail/DetailScreen";
 
 // ? If you want to use stack or tab or both
 const Tab = createBottomTabNavigator();
@@ -15,7 +16,28 @@ const Stack = createStackNavigator();
 const Navigation = (props) => {
   const renderTabNavigation = () => {
     return (
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = focused ? "ios-heart" : "ios-heart-empty";
+            } else if (route.name === "Search") {
+              iconName = focused ? "ios-search" : "ios-search";
+            }
+
+            // You can return any component that you like here!
+            return (
+              <Icon name={iconName} type="Ionicons" size={size} color={color} />
+            );
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: "tomato",
+          inactiveTintColor: "gray",
+        }}
+      >
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Search" component={SearchScreen} />
       </Tab.Navigator>
